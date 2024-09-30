7d76dc2f3e42506ca063131510e5dc89e16f2d21
許文鑫 <xuwenxin@xuwenxindeMacBook-Air.local>
2024-09-30 17:55:40 +0800

    fix error when k>1

--- course-rag-prototype/streamlit_app.py
+++ course-rag-prototype/streamlit_app.py
@@ -23,7 +23,7 @@ async def get_answer_multilingual_e5(query: str) -> str:
     vectorstore = PineconeVectorStore(
         index_name=index_name, embedding=embeddings)
     docs = await asyncio.to_thread(vectorstore.similarity_search, query=query, k=k)
-    chain = load_qa_chain(llm, chain_type="map_reduce")
+    chain = load_qa_chain(llm, chain_type="stuff")
     answer = await asyncio.to_thread(chain.run, input_documents=docs, question=query)
     return answer
 
