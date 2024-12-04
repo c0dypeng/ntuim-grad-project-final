5c5a588125450730307f72eba50cf193a17f549d
ansonwuuuuu <ansonwu0604@gmail.com>
2024-12-04 18:48:52 +0800

    feat: add metadata filter

--- course-rag-prototype/function/simple_rag_agent.py
+++ course-rag-prototype/function/simple_rag_agent.py
@@ -1,5 +1,5 @@
 from langchain_pinecone import PineconeVectorStore
-from langchain.agents import initialize_agent
+# from langchain.agents import initialize_agent
 from langchain.tools import BaseTool
 from langchain.prompts import PromptTemplate
 from langchain.agents.agent_types import AgentType
@@ -14,6 +14,7 @@ from langchain.schema import Document
 
 def prepare_documents_with_separation(docs):
     prepared_docs = []
+    printOnce = False
     for i, doc in enumerate(docs, 1):
         metadata_str = ', '.join(f"{key}: {value}" for key, value in doc.metadata.items())
         formatted_content = (
@@ -21,6 +22,9 @@ def prepare_documents_with_separation(docs):
             f"Metadata: {metadata_str}\n"
             f"Content:\n{doc.page_content}\n"
         )[:700]
+        if not printOnce:
+            print(formatted_content)
+            printOnce = True
         prepared_docs.append(Document(page_content=formatted_content, metadata=doc.metadata))
     return prepared_docs
 
@@ -32,14 +36,8 @@ class VectorStoreSearchTool(BaseTool):
 
     def _run(self, query: str) -> str:
         docs = self.vectorstore.similarity_search(query=query, k=self.k)
-        return "\n\n".join([doc.page_content for doc in docs])
-    
-    async def _arun(self, query: str) -> str:
-        docs = await asyncio.to_thread(
-            self.vectorstore.similarity_search, query=query, k=self.k
-        )
         reordering = LongContextReorder()
-        docs = reordering.reorder(docs)
+        docs = reordering.transform_documents(docs)
 
         # data filtering
         # make embedding_text in matadata to be empty
@@ -48,9 +46,28 @@ class VectorStoreSearchTool(BaseTool):
         # make text in matadata to be empty
         for doc in docs:
             doc.metadata["text"] = ""
-
+        
+        print("Im here")
         docs = prepare_documents_with_separation(docs)
+        return "\n\n".join([doc.page_content for doc in docs])
+    
+    async def _arun(self, query: str) -> str:
+        docs = await asyncio.to_thread(
+            self.vectorstore.similarity_search, query=query, k=self.k
+        )
+        reordering = LongContextReorder()
+        docs = reordering.transform_documents(docs)
 
+        # data filtering
+        # make embedding_text in matadata to be empty
+        for doc in docs:
+            doc.metadata["embedding_text"] = ""
+        # make text in matadata to be empty
+        for doc in docs:
+            doc.metadata["text"] = ""
+        
+        print("Im here")
+        docs = prepare_documents_with_separation(docs)
         return "\n\n".join([doc.page_content for doc in docs])
 
 async def get_answer_simple_rag_agent(embedding, llm, k, query: str) -> str:    
